2f7649a3eea35de77f83b515f796902f4d8f4241
James Browne <2509941b@student.gla.ac.uk>
2022-01-28 12:18:33 +0000

    finished 6th chapter

--- populate_rango.py
+++ populate_rango.py
@@ -1,6 +1,5 @@
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-'tango_with_django_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
 
 import django
 django.setup()
@@ -14,28 +13,37 @@ def populate():
     # through each data structure, and add the data to our models.
 
     python_pages = [
-        {'title': 'Official Python Tutorial',
-        'url':'http://docs.python.org/3/tutorial/'},
-        {'title':'How to Think like a Computer Scientist',
-        'url':'http://www.greenteapress.com/thinkpython/'},
-        {'title':'Learn Python in 10 Minutes',
-        'url':'http://www.korokithakis.net/tutorials/python/'} ]
+        {"title": "Official Python Tutorial",
+        "url":"http://docs.python.org/3/tutorial/",
+        "views": 50},
+        {"title":"How to Think like a Computer Scientist",
+        "url":"http://www.greenteapress.com/thinkpython/",
+        "views": 10},
+        {"title":"Learn Python in 10 Minutes",
+        "url":"http://www.korokithakis.net/tutorials/python/",
+        "views": 40} ]
 
     django_pages = [
-        {'title':'Official Django Tutorial',
-        'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
-        {'title':'Django Rocks',
-        'url':'http://www.djangorocks.com/'},
-        {'title':'How to Tango with Django',
-        'url':'http://www.tangowithdjango.com/'} ]
+        {"title":"Official Django Tutorial",
+        "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/",
+        "views": 6},
+        {"title":"Django Rocks",
+        "url":"http://www.djangorocks.com/",
+        "views": 7},
+        {"title":"How to Tango with Django",
+        "url":"http://www.tangowithdjango.com/",
+        "views": 30} ]
 
     other_pages = [
-        {'title':'Bottle',
-        'url':'http://bottlepy.org/docs/dev/'},
-        {'title':'Flask',
-        'url':'http://flask.pocoo.org'} ]
+        {"title":"Bottle",
+        "url":"http://bottlepy.org/docs/dev/",
+        "views": 4},
+        {"title":"Flask",
+        "url":"http://flask.pocoo.org",
+        "views": 20} ]
 
-    cats = {"Python": {"pages": python_pages, "views":128, "likes":64},
+    cats = {
+            "Python": {"pages": python_pages, "views":128, "likes":64},
             "Django": {"pages": django_pages, "views":64, "likes":32},
             "Other Frameworks": {"pages": other_pages, "views":32, "likes":16}
             }
@@ -48,7 +56,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], p['views'])
 
     # Print out the categories we have added.
     for c in Category.objects.all():
